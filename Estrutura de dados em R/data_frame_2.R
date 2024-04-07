cee4093268426747622385d9976e8ce9e71b09bb
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-07 18:47:32 -0300

    Create data_frame_2.R

---
+++ Estrutura de dados em R/data_frame_2.R
@@ -0,0 +1,15 @@
+empresa <- data.frame(
+  codigos = c(1:5),
+  funcionarios = c('Joao','Pedro','Antonio','Celso','Maria'),
+  salarios = c(1800,2500,3000,1200,2300),
+  data_inicio = c('01/01/2020','23/09/2013','15/11/2022',
+                  '11/05/2018','27/03/2019')
+)
+empresa
+empresa
+  codigos funcionarios salarios data_inicio
+1       1         Joao     1800  01/01/2020
+2       2        Pedro     2500  23/09/2013
+3       3      Antonio     3000  15/11/2022
+4       4        Celso     1200  11/05/2018
+5       5        Maria     2300  27/03/2019
