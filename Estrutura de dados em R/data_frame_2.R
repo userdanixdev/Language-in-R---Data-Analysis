5047b55c3da7fa03abf9f626173a2c0aa65ce210
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-07 18:57:06 -0300

    Update data_frame_2.R

--- Estrutura de dados em R/data_frame_2.R
+++ Estrutura de dados em R/data_frame_2.R
@@ -13,3 +13,23 @@ empresa
 3       3      Antonio     3000  15/11/2022
 4       4        Celso     1200  11/05/2018
 5       5        Maria     2300  27/03/2019
+
+--------//-----------------------//-----------------------//----------------------//
+concessionaria <-data.frame(
+  Códigos = c(1:6),
+  Marca = c('Chevrolet','Fiat','Fiat','Ford','Volkswagen','Fiat'),
+  Modelo = c('Onix','Uno','Toro','KA','Gol','Palio'),
+  Ano = c(2018,2007,2019,2010,2009,2011))
+
+
+Resultado:>
+concessionaria
+  Códigos      Marca Modelo  Ano
+1       1  Chevrolet   Onix 2018
+2       2       Fiat    Uno 2007
+3       3       Fiat   Toro 2019
+4       4       Ford     KA 2010
+5       5 Volkswagen    Gol 2009
+6       6       Fiat  Palio 2011
+
+  
