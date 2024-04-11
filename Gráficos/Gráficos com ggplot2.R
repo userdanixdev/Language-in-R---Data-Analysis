aa43d5b8c7fafe54e6dfc2af23f38c989536fda5
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-11 14:21:19 -0300

    Create Gráficos com ggplot2.R

---
+++ Gráficos/Gráficos com ggplot2.R
@@ -0,0 +1,10 @@
+install.packages ("ggplot2")
+install.packages ("esquisse")
+
+# Carregando os pacotes:
+library(esquisse)
+library(ggplot2)
+
+# Dataset nativo do R : DIAMONDS
+diamonds = diamonds 
+
