864fddf941b813913ac7aa5f18c8a09ae82765d3
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-11 17:18:28 -0300

    Update Gráficos com ggplot2.mpg.R

--- Gráficos/Gráficos com ggplot2.mpg.R
+++ Gráficos/Gráficos com ggplot2.mpg.R
@@ -21,4 +21,32 @@ ggplot(mpg) +
  plot.subtitle = element_text(face = "italic", hjust = 0.5))
 
 ------------//-----------------//---------------------------//----------------//----------------//-------------------
+# Editando e aprofundando sobre gráfico ggplot:
+
+ggplot(mpg) +
+  aes(x = displ, y  = hwy,fill=drv)+
+  geom_point(alpha = 0.8, # <- TRANSPARÊNCIA
+             colour = 'red',
+             #shape = 24,
+             size=2,
+             shape = 24)  +
+  labs(x = "Cilindradas no Motor", y = "Consumo na Estrada (milhas x galão)", 
+       title = "Relação entre consumo e cilindradas do motor",
+       subtitle = "O consumo em milhas diminiu quanto mais cilindros tem o motor", 
+       caption = "Pacote: ggplot2",fill='Tração') +
+  theme_bw()+
+  theme(plot.title=element_text(face='bold',colour='red',hjust=0.5,
+                                margin=margin(unit='cm')))+
+  theme(plot.subtitle =element_text(hjust=0.5,face='italic'),
+        plot.caption=element_text(face='italic'),
+        axis.title.x=element_text(color='blue'),
+        legend.background=element_rect(fill='yellow',linetype = 2),
+        legend.title=element_text(face='bold'),
+        plot.background = element_rect(fill='gray'),
+        panel.background = element_rect(fill='yellow'),
+        panel.grid=element_line(colour='gray'),
+        panel.border=element_line(face='bold')
+        )
+        
+
 
