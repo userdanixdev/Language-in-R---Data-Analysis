44c78a46db53a09941005a0f1f09aecbd79adb1e
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-19 12:57:57 -0300

    Create Inferência.Proporção.r

---
+++ Análise Estatística/Inferência.Proporção.r
@@ -0,0 +1,10 @@
+# Inferência sobre a proporção da população
+# Inferência QUALITATIVA:
+
+vota = c("Sim","Nao","Nao","Sim","Nao","Sim",
+         "Sim","Nao","Sim","Sim","Nao","Sim",
+         "Sim","Sim","Nao","Sim","Nao","Sim",
+         "Sim","Nao","Sim","Sim","Sim","Nao",
+         "Nao","Sim","Sim","Sim","Nao","Sim")
+eleicoes=data.frame(vota)
+
