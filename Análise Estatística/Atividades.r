71c887f05755c473cbfe2206a6f254f8be15d32a
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-23 17:03:45 -0300

    Create Atividades.r

---
+++ Análise Estatística/Atividades.r
@@ -0,0 +1,13 @@
+
+Analistas do mercado de capitais são profissionais que emitem recomendações de compra ou de venda de ações que podem ser
+utilizadas por diversos interessados. Há vários estudos sobre os analistas, 
+entre eles aqueles que buscam identificar variáveis que influenciam na acurácia de suas recomendações.
+
+Um desses estudos pode ser conferido, na íntegra, nesta página da internet.
+Parte da base de dados utilizada no estudo está disponível no arquivo "analistasMC", disponível neste link.
+
+Esta atividade utilizará as seguintes variáveis:
+
+ACUR: medida de acurácia dos analistas - quanto mais próxima de zero, menos os analistas erram em suas previsões;
+logVM: logarítimo do valor de mercado da empresa, como medida do seu tamanho.
+
