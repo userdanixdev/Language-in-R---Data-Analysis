83af6073297b0b67a9abdb949d2dfd597f285134
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-24 14:40:10 -0300

    Create Regr_linear_multipla.r

---
+++ Análise Estatística/Regr_linear_multipla.r
@@ -0,0 +1,10 @@
+# No estudo sobre autonomia dos veículos, percebemos uma relação entre o número de cilindros dos caros e a distância
+# que conseguem percorrer com um galão de combustível. Vamos acrescentar a variável tipo de TRANSMISSÃO (am).
+
+# O modelo ficará assim:
+  mpg = β₀+β₁am+β₂cyl + erro
+
+# Para testar as hipóteses temos:
+    # Hipótese 0 :: NULA :: H0 :: não há relação entre as variáveis am e mpg
+    # Hipótese 1 :: Alternativa :: H1 :: as variáveis am e mpg estão relacionadas.
+
