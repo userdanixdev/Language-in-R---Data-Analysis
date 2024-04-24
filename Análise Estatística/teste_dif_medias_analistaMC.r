9cad98c464ad532fea930e22b83e1eb3b754ab82
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-24 20:09:25 -0300

    Update teste_dif_medias_analistaMC.r

--- Análise Estatística/teste_dif_medias_analistaMC.r
+++ Análise Estatística/teste_dif_medias_analistaMC.r
@@ -11,5 +11,24 @@ Antes de 2008    -0.7269017 1.258975 0.5261447 -13.58503 -0.6679487 -0.2793301 -
 
 # Na amostra, a média da acurácia antes de 2018 é maior que a média da acurácia a partir de 2018. [VERDADEIRO]
 
+t.test(data=analistasMC,ACUR~ANO,alternative='two.sided',conf.level=.95,var.equal=TRUE)
+Two Sample t-test
+
+data:  ACUR by ANO
+t = -0.30877, df = 1031, p-value = 0.7576
+alternative hypothesis: true difference in means between group A partir de 2008 and group Antes de 2008 is not equal to 0
+95 percent confidence interval:
+ -0.1859795  0.1354084
+sample estimates:
+mean in group A partir de 2008    mean in group Antes de 2008 
+                    -0.7521873                     -0.7269017 
+
+O p-value é maior que 5%, indicando que a hipótese nula deve ser rejeitada.[FALSO] Indica que a hipótese nula não deve ser rejeitada.
+
+O número zero não aparece no intervalo de confiança, motivo pelo qual a hipótese nula deve ser rejeitada. [FALSO]
+[INDICANDO QUE A HIPÓTESE NULA NÃO DEVE SER REJEITADA]
+
+
+
 
 
