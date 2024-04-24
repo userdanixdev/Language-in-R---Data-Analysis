0b78b5fa95147e29a789b96b2fa244f38766277e
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-24 17:13:42 -0300

    Create Premissas_testes.r

---
+++ Análise Estatística/Premissas_testes.r
@@ -0,0 +1,30 @@
+# Para que os resultados dos testes estatisticos sejam robustos, os dados precisam respeitar algumas premissas:
+
+Premissa para aleatoriedade é base para todos os testes estatisticos:
+    1. Uma amostra
+    2. Diferença de médias
+    3. ANOVA
+    4. Regressão Simples 
+    5. Regressão Múltipla
+
+Premissa de NORMALIDADE:
+    1. Uma amostra
+    2. Diferença de médias
+    3. ANOVA
+    4. Regressão Simples 
+    5. Regressão Múltipla
+
+Premissa da independência:
+
+    1. Para diferenças de médias & ANOVA
+
+Premissa de variâncias iguais:
+    1. Diferença de médias 
+    2. ANOVA
+    3. Regressão simples e múltipla
+Premissa de LINEARIDADE:
+    1. Para regressão simples e múltipla.
+Premissa de autocorrelação:
+    1. Regressão Simples e Múltipla
+Premissa de Multicolinearidade:
+    1. Regressão Múltipla
